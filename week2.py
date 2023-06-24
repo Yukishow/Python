4b612f8f5680eee4f9e84b150a82fc85631afb30
Yukishow <g0919397742@gmail.com>
2023-06-25 00:11:03 +0800

    week1 wee2 file upload

---
+++ week2.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        result = 0
+        buy = prices[0]
+        for sell in prices[1:]:
+            if sell > buy:
+                result = max(result, sell - buy)
+            else:
+                buy = sell
+        return result
